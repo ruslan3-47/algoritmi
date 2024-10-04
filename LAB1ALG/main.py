c34730445ded7509f8a8a946befbc885356503b7
ruslan3-47 <vip.artyxa@gmail.com>
2024-10-04 23:39:21 +0300

    add bfs fucntion, empty for queue

--- LAB1ALG/main.py
+++ LAB1ALG/main.py
@@ -1,3 +1,4 @@
+#АТД «очередь» - задача коммивояжера, исчерпывающий поиск в ширину
 class Queue: #создание_очереди
     def __init__(self):
         self.way = []
@@ -7,7 +8,22 @@ class Queue: #создание_очереди
         return self.way.pop()
     def size(self):
         return len(self.way)
-class cities:
-    def __init__(self, name, connect):
-        self.name= name
-        self.connect = connect
+    def empty(self):
+        return len(self.way)==0
+def bfs(graph, beggin):
+    queue = Queue()  # не посещенные вершины
+    queue.add(beggin)
+    visited = set()  # посещенные
+    visited.add(beggin)  # добавляем начальную вершину
+    while queue.empty()!=1:
+        v = queue.remove()
+        print(v,end=' ')
+        for vertex in graph[v]:#осматриваем окрестности вершин
+            if vertex not in visited:
+                visited.add(vertex)
+                queue.add(vertex)
+
+graph = {0:[1, 3],1:[0, 2],2:[0,1,4],3:[0],4:[2]} #вершины и связи
+bfs(graph,0)
+
+
